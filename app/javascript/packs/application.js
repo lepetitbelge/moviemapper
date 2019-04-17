e6adca926551d90f993e3c40a11f3f0e60f83c88
christophegillis <chris.tim.gillis@gmail.com>
2019-04-17 16:41:40 +0200

    added moviesearch file in js/components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../packs/map';
-
 initMapbox();
+
+import { initMovieSearch } from '../components/movie_search';
+initMovieSearch();
