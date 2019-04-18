f6a3c8d090c44da68454a562410b441e13a75fbb
christophegillis <chris.tim.gillis@gmail.com>
2019-04-18 18:50:13 +0200

    added seeds for films, locations and filmlocations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,27 @@ MovieLocation.destroy_all
 # creates 20 movies
 # creates 100 locations
 # links each movie with 3-10 locations
+puts "creating 100 locations"
+100.times do
+  location = Location.create(
+    longitude: Faker::Address.longitude,
+    latitude: Faker::Address.latitude,
+    address: Faker::Address.full_address
+    )
+end
+puts "creating 20 movies"
 20.times do
   movie = Movie.create(
-
+    title: Faker::Book.title,
+    year: rand(1950..2019),
+    rating: rand(0..10)
     )
+end
 
+puts "creating 200 links"
+200.times do
+  movie_location = MovieLocation.create(
+    movie_id: Movie.all.sample.id,
+    location_id: Location.all.sample.id
+    )
 end
