caf6577640d2200d23b5a9b1f5a5697d5a1d7102
christophegillis <chris.tim.gillis@gmail.com>
2019-04-18 16:56:07 +0200

    added validations for models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ApplicationRecord
+  has_many :movie_locations
+  has_many :movies, through: :movie_locations
+
+  validates :address, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+  has_many :movie_locations
+  has_many :locations, through: :movie_locations
+
+  validates :title, uniqueness: { message: "title is already taken" }
+  validates :year, presence: { message: "must be given please" }, numericality: true
+  validates :rating, numericality: { message: "did you put a number?" }
 end
