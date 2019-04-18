19b47799ac4f18b31f96b5bb760fb3736677cb31
christophegillis <chris.tim.gillis@gmail.com>
2019-04-18 10:27:08 +0200

    updated fetch method for movieAPI

--- app/javascript/components/movie_search.js
+++ app/javascript/components/movie_search.js
@@ -1,5 +1,10 @@
-
-
-
+const initMovieSearch = () => {
+  const url = "https://www.myapifilms.com/imdb/idIMDB?title=harry+potter&token=996c2225-65cb-46b4-a895-043584a46968&format=json&language=en-us&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=3&exactFilter=0&limit=1&forceYear=0&trailers=0&movieTrivia=0&awards=0&moviePhotos=0&movieVideos=0&actors=1&biography=0&actorActress=0&similarMovies=0&adultSearch=0&goofs=0&keyword=0&quotes=0&fullSize=0&companyCredits=0&filmingLocations=2";
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
 export { initMovieSearch };
