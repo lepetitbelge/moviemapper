b421a283256a7564e283b38894f6719f1fcf2411
christophegillis <chris.tim.gillis@gmail.com>
2019-04-18 17:33:32 +0200

    added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#destroy all records
+Movie.destroy_all
+Location.destroy_all
+MovieLocation.destroy_all
+
+# creates 20 movies
+# creates 100 locations
+# links each movie with 3-10 locations
