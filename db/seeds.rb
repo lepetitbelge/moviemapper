df2f6699189ba5f3c622cb8b8939b6cdb7f7430f
christophegillis <chris.tim.gillis@gmail.com>
2019-04-26 11:00:15 +0200

    in MOVIE changed rating from int to float and added presence to title

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ MovieLocation.destroy_all
 # links each movie with 3-10 locations
 puts "creating 100 locations"
 100.times do
-  location = Location.create(
+  location = Location.create!(
     longitude: Faker::Address.longitude,
     latitude: Faker::Address.latitude,
     address: Faker::Address.full_address
@@ -16,7 +16,7 @@ puts "creating 100 locations"
 end
 puts "creating 20 movies"
 20.times do
-  movie = Movie.create(
+  movie = Movie.create!(
     title: Faker::Book.title,
     year: rand(1950..2019),
     rating: rand(0..10)
@@ -25,7 +25,7 @@ end
 
 puts "creating 200 links"
 200.times do
-  movie_location = MovieLocation.create(
+  movie_location = MovieLocation.create!(
     movie_id: Movie.all.sample.id,
     location_id: Location.all.sample.id
     )
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Movie do
   context "When testing, the movie class" do
