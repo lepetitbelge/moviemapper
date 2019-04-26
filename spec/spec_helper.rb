ae070882b585f0246a813d345f6fd75064ca5948
christophegillis <chris.tim.gillis@gmail.com>
2019-04-26 10:05:53 +0200

    added required paths

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+require File.expand_path("../../config/environment", __FILE__)
+require_relative '../app/models/user.rb'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
